57adfd0bf1eed41b3ca8fcdefb3434a62cbb4671
ShihabKuet <shihabkuetcse@gmail.com>
2026-04-26 16:23:16 +0600

    feat(icons): add multiple new icon components including MailIcon, SwordsIcon, TelescopeIcon, TerminalIcon, UserStarIcon, WebhookIcon, and ZapIcon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,3 +1,10 @@
 export { PenToolIcon, type PenToolIconHandle } from "./PenToolIcon";
 export { ScanTextIcon, type ScanTextIconHandle } from "./ScanTextIcon";
+export { TelescopeIcon, type TelescopeIconHandle } from "./TelescopeIcon";
+export { ZapIcon, type ZapHandle } from "./ZapIcon";    
+export { SwordsIcon, type SwordsIconHandle } from "./SwordsIcon";
+export { TerminalIcon, type TerminalIconHandle } from "./TerminalIcon";
+export { MailIcon, type MailIconHandle } from "./MailIcon";
+export { WebhookIcon, type WebhookHandle } from "./WebhookIcon";
+export { UserStarIcon, type UserStarHandle} from "./UserStarIcon";   
 // ... add every icon here once
