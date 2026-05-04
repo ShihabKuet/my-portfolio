668669bf3f458e05cecd823ff1f56afa81a9ad00
Sanzid Arefin Shihab <arefin1807104@stud.kuet.ac.bd>
2026-05-04 18:32:39 +0600

    Fix Facebook link share - OG image not appear

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,17 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/api/"],   // no need for Google to crawl your API routes
-    },
+    rules: [
+      {
+        userAgent: "facebookexternalhit",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/"], // no need for Google to crawl your API routes
+      },
+    ],
     sitemap: "https://shanjid.bd/sitemap.xml",
   };
 }
